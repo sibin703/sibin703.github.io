37b50f5947572be5e0abf025fa0e79ca61373854
Sibin Mathew Varghese <smathewvarghese6157@conestogac.ca.on>
2023-04-19 07:54:42 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+function playVideo1() {
+    document.getElementById("video1").style.display = "block";
+    document.getElementById("video2").style.display = "none";
+}
+function playVideo2() {
+    document.getElementById("video1").style.display = "none";
+    document.getElementById("video2").style.display = "block";
+}
+function playVideo3() {
+    document.getElementById("video1").style.display = "none";
+    document.getElementById("video2").style.display = "none";
+    document.getElementById("video3").style.display = "block";
+}
+function playVideo4() {
+    document.getElementById("video1").style.display = "none";
+    document.getElementById("video2").style.display = "none";
+    document.getElementById("video3").style.display = "none";
+    document.getElementById("video4").style.display = "block";
+}
+function playVideo5() {
+    document.getElementById("video1").style.display = "none";
+    document.getElementById("video2").style.display = "none";
+    document.getElementById("video3").style.display = "none";
+    document.getElementById("video4").style.display = "none";
+    document.getElementById("video5").style.display = "block";
+}
+function playVideo6() {
+    document.getElementById("video1").style.display = "none";
+    document.getElementById("video2").style.display = "none";
+    document.getElementById("video3").style.display = "none";
+    document.getElementById("video4").style.display = "none";
+    document.getElementById("video5").style.display = "none";
+    document.getElementById("video6").style.display = "block";
+}
+function playVideo7() {
+    document.getElementById("video1").style.display = "none";
+    document.getElementById("video2").style.display = "none";
+    document.getElementById("video3").style.display = "none";
+    document.getElementById("video4").style.display = "none";
+    document.getElementById("video5").style.display = "none";
+    document.getElementById("video6").style.display = "none";
+    document.getElementById("video7").style.display = "block";
+}   
+
+function fn1() {
+    document.getElementById("displaymsg").innerHTML = "<p>Success! Your Message has been sent.</p>"
+}
